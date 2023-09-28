6f6162a6b0338b9d6fb2ca73aef2e8f610a195ec
AbinSinghK <141057162+AbinSinghK@users.noreply.github.com>
2023-09-28 15:56:52 +0530

    terraform code

--- main.tf
+++ main.tf
@@ -1,1 +1,17 @@
-hi
+provider "aws" {
+  region = "ap-south-1" # Change to your desired AWS region
+}
+
+resource "aws_instance" "abins_instance" {
+  ami           = "ami-067c21fb1979f0b27" # Specify your desired AMI ID
+  instance_type = "t2.micro"              # Specify your desired instance type
+  key_name      = "mumbai"    # Specify your key pair name
+
+  subnet_id     = "subnet-xxxxxxxxxxxxxx" # Specify your desired subnet ID
+
+  security_groups = ["all"] # Specify your security group(s) by name
+}
+
+output "instance_public_ip" {
+  value = aws_instance.abins_instance.public_ip
+}
